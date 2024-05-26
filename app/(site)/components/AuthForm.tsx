5889e9e64b56f6428470b7c9e339136315fd092e
noorimanr@gmail.com <noorimanr@gmail.com>
2024-05-26 23:44:12 +0800

    auth UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -47,6 +48,7 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}></form>
+        <Input id="email" label="Email" register={register} errors={errors} />
       </div>
     </div>
   );
