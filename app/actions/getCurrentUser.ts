0448689f18cee854ecca7aef08bc1977965b2c4b
noorimanr@gmail.com <noorimanr@gmail.com>
2024-06-04 23:03:45 +0800

    feat: message & image sender

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -9,7 +9,7 @@ const getCurrentUser = async () => {
     }
     const currentUser = await prisma.user.findUnique({
       where: {
-        email: session.user.email as string,
+        email: session.user.email,
       },
     });
     if (!currentUser) {
